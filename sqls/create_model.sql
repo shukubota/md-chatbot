31bb0bf14d39879a8f43e298dfb054e94534f75f
shukubota <kubotashu0421@gmail.com>
2024-11-07 22:59:07 +0900

    vector検索

--- sqls/create_model.sql
+++ sqls/create_model.sql
@@ -20,3 +20,35 @@ CREATE OR REPLACE TABLE
       FROM
         `bigquery-public-data.imdb.reviews` ),
       STRUCT(TRUE AS flatten_json_output) ) );
+
+-- ベクトル検索
+DECLARE question_text STRING 
+  DEFAULT "映画のレビューを教えてください";
+
+WITH
+  -- 質問文をベクトル化する
+  embedded_question AS (
+  SELECT
+    *
+  FROM
+    ML.GENERATE_TEXT_EMBEDDING( MODEL `gig-sandbox-ai.example_dataset.example_embedding`,
+      (SELECT question_text AS content),
+      STRUCT(TRUE AS flatten_json_output)
+    )
+  ),
+  -- ベクトル化された FAQ データを取得する
+  embedded_faq AS (
+  SELECT
+    *
+  FROM
+    `gig-sandbox-ai.example_dataset.example_embedded_reviews` )
+-- ベクトル距離（コサイン類似度）を算出する
+SELECT
+  q.content as question,
+  f.content as reference,
+  ML.DISTANCE(q.text_embedding, f.text_embedding, 'COSINE') AS vector_distance
+FROM
+  embedded_question AS q,
+  embedded_faq AS f
+ORDER BY
+  vector_distance
